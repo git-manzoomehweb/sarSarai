e48f1deb12fbad0f32c64ecb3ac06bfbce9866fc
bita <kmg.d142@gmail.com>
2025-09-20 17:33:38 +0330

    Refactor code structure

---
+++ Design/mobile/src/js/sarsara-swipper-functions.js
@@ -0,0 +1,37 @@
+var swiper1 = new Swiper(".swiper-tours1", {
+    slidesPerView: 1,
+    speed: 400,
+   
+    centeredSlides: 0,
+    spaceBetween: 30,
+    grabCursor: !0,
+    autoplay: 0,
+    loop: !0,
+    pagination: {
+    el: ".swiper-pagination1",
+    clickable: !0,
+    },
+    navigation: {
+        nextEl: '',
+        prevEl: '',
+    },
+});
+
+var swiper2 = new Swiper(".swiper-tours2", {
+        slidesPerView: 1,
+        speed: 400,
+       
+        centeredSlides: 0,
+        spaceBetween: 30,
+        grabCursor: !0,
+        autoplay: 0,
+        loop: !0,
+        pagination: {
+        el: ".swiper-pagination2",
+        clickable: !0,
+        },
+        navigation: {
+            nextEl: '',
+            prevEl: '',
+        },
+});
---
+++ Design/pc/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "var(--primary)",
+          700: "var(--primary-700)",
+          600: "var(--primary-600)",
+          500: "var(--primary-500)",
+          300: "var(--primary-300)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          600: "var(--secondary-600)",
+          500: "var(--secondary-500)",
+          800: "var(--secondary-800)",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
